7b93bb28b8183c70524984264f1b23889ff223f7
proxod3 <63583253+proxod3-first@users.noreply.github.com>
2023-10-09 14:39:32 +0300

    modified:   tests.py

--- tests.py
+++ tests.py
@@ -1,33 +1,13 @@
 import unittest
-from game import choose_word, display_word
+from game import display_word
 
 
 class TestGameFunctions(unittest.TestCase):
-    def test_choose_word(self):
-        words = ["apple", "banana", "cherry", "date", "wildberry"]
-        chosen_word = choose_word()
-        self.assertIn(chosen_word, words)
-
-    def test_choose_word2(self):
-        words = ["app"]
-        chosen_word = choose_word()
-        self.assertNotIn(chosen_word, words)
-
     def test_display_word(self):
         word = "apple"
         guessed_letters = ["a", "p"]
         displayed_word = display_word(word, guessed_letters)
         self.assertEqual(displayed_word, "app__")
-
-    def test_display_word2(self):
-        word = "cherry"
-        guessed_letters = ["c", "h"]
-        displayed_word = display_word(word, guessed_letters)
-        self.assertEqual(displayed_word, "ch____")
-
-    def test_play_game(self):
-        # Test the game logic
-        pass
 
 
 if __name__ == '__main__':
